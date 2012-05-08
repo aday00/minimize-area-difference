ee75024b6b574cfe44d162883fdbabdeab0a338a
Andrew Schaumberg <schaumberga@njhealth.org>
2012-05-07 20:28:56 -0600

    Calcs mean of expected dat, shifts expected dat by the mean to the center. Calcs mean of experimental dat, shifts experimental dat by the mean to the center. Calculates the difference between these centered dats.  This is less than uncentered difference. This helps figure out the bias function, where experimental[i] = bias(expected[i], expected_mean, experimental_mean) Eventually, this bias function will correct for amplitude and spread, I think. Eventually the bias function will be found iteratively, finding progressively better means, amplitudes, and spreads to minimize error.
    expected mean:     50.0
    experimental mean: 40.0
    expected:experimental diff, unadjusted: 1.61299698890372
    expected:experimental diff, centered:   1.42111290910789e-263

--- debias.rb
+++ debias.rb
@@ -6,6 +6,7 @@ $experimental_dat_file = "norm-distrib_mean40_variance0.2_amp1.0_range30-70.dat"
 $acceptable_error = 1.0 # acceptable total area difference between both dats
 
 #constants
+$acceptable_error_squared = $acceptable_error**2
 $dat_filter = /^([-0-9]+)\s+([-_.e0-9]+)$/
 
 def dat_read(filename)
@@ -41,14 +42,48 @@ puts "expected mean:     #{$expected_mean}"
 puts "experimental mean: #{$experimental_mean}"
 
 def dat_diff(expected, experimental)
-  diffsquares = 0.0
-  expected_pt     = nil
-  experimental_pt = nil
-  0.upto(expected.length - 1) do |i|
-    expected_pt     = expected[i][1]
-    experimental_pt = experimental[i][1]
-    diffsquares += (expected_pt - experimental_pt)**2
+  diffsquaressum = 0.0
+
+  expected_amt = experimental_amt = nil
+
+  min_gc = [expected[0][0],  experimental[0][0]].min
+  max_gc = [expected[-1][0], experimental[-1][0]].max
+
+  expected_idx = experimental_idx = 0
+
+  min_gc.upto(max_gc) do |gc|
+    if (expected_idx < expected.length and
+         gc == expected[expected_idx][0])
+      expected_amt = expected[expected_idx][1]
+      expected_idx += 1
+    else
+      expected_amt = 0
+    end
+
+    if (experimental_idx < experimental.length and
+        gc == experimental[experimental_idx][0])
+      experimental_amt = experimental[experimental_idx][1]
+      experimental_idx += 1
+    else
+      experimental_amt = 0
+    end
+
+    diffsquaressum += (expected_amt - experimental_amt)**2
   end
-  return Math.sqrt(diffsquares)
+  return diffsquaressum # sum of the squared differences, measures fit, should minimize for best
 end
-puts "diff: #{dat_diff($expected_dat, $experimental_dat)}"
+puts "expected:experimental diff, unadjusted: #{dat_diff($expected_dat, $experimental_dat)}"
+
+def dat_shift_to_center(dat, mean)
+  shifted = []
+  dat.each do |d|
+    gc_content_adjusted = (d[0] - mean).to_i
+    gc_content_amount   = d[1]
+    shifted << [gc_content_adjusted, gc_content_amount]
+  end
+  return shifted
+end
+expected_centered     = dat_shift_to_center($expected_dat,     $expected_mean)
+experimental_centered = dat_shift_to_center($experimental_dat, $experimental_mean)
+centered_diff = dat_diff(expected_centered, experimental_centered)
+puts "expected:experimental diff, centered:   #{dat_diff(expected_centered, experimental_centered)}"
