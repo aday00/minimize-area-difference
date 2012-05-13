524f0ac6a940a641b23fc369317095b601155090
Andrew Schaumberg <schaumberga@njhealth.org>
2012-05-13 16:39:54 -0600

    Linear interpolation between each datapoint, so that fit between expected and experimental may be shifted by a fraction of a bin. This fractional shifting along the X axis (gc_content bin) should allow for greater error minimization, as fractional shifts can shift the experimental curve "tighter" to the expected curve, so a greater amount of areal overlap is possible.
    Also printing out resulting datapoints so it's easy to plug into
    Excel, or make another gnuplot.
    
    Output:
    expected mean:     50.0
    experimental mean: 40.0
    expected:experimental diff, unadjusted: 1.32265753090105
    expected:experimental diff, centered:   0.0322599397780744
    expected_gc_content[i] =~ experimental_gc_content[i + (10.0)]
    expected:experimental diff, found:      0.233387167361045
    expected_gc_content[i] =~ experimental_gc_content[i + (10.0)]
    expected     max: 0.892062058076386
    experimental max: 0.713649646461109
    expected:experimental amplification: 1.25
    expected:experimental diff, amplified:  6.641177678478
    expected_gc_content[i] =~ experimental_gc_content[i + (10.0)] * (1.25)
    debiased data follows according to above linear transformation:
    39	0.0
    40	2.38108331707304e-109
    41	1.01335702507848e-88
    42	2.90588192420119e-70
    43	5.61462861653231e-54
    44	7.30956776526076e-40
    45	6.41194737115056e-28
    46	3.78979564041296e-18
    47	1.50927794290321e-10
    48	4.04995547804455e-05
    49	0.0732249128096324
    50	0.892062058076386
    51	0.0732249128096324
    52	4.04995547804455e-05
    53	1.50927794290321e-10
    54	3.78979564041296e-18
    55	6.41194737115056e-28
    56	7.30956776526076e-40
    57	5.61462861653231e-54
    58	2.90588192420119e-70
    59	1.01335702507848e-88
    60	2.38108331707304e-109
    61	3.76976512412629e-132
    62	4.02143990200897e-157
    63	2.89052322923912e-184
    64	1.39990619491475e-213
    65	4.56824089617716e-245
    66	1.00444613019402e-278
    67	1.48809923297984e-314
    68	0.0
    69	0.0
    70	0.0
    71	0.0
    72	0.0
    73	0.0
    74	0.0
    75	0.0
    76	0.0
    77	0.0
    78	0.0
    79	0.0

--- debias.rb
+++ debias.rb
@@ -8,6 +8,9 @@ $acceptable_error = 1.0 # acceptable total area difference between both dats
 #constants
 $acceptable_error_squared = $acceptable_error**2
 $dat_filter = /^([-0-9]+)\s+([-_.e0-9]+)$/
+#$finesteps = 100 # when fine-tuning expected-to-experimental fit, use this many steps
+$finesteps = 10 # when fine-tuning expected-to-experimental fit, use this many steps
+$halffine = $finesteps / 2.0
 
 def dat_read(filename)
   dat = []
@@ -41,11 +44,41 @@ $experimental_mean = dat_mean($experimental_dat)
 puts "expected mean:     #{$expected_mean}"
 puts "experimental mean: #{$experimental_mean}"
 
+def dat_interpolate(dat)
+  # multiple the number of data points in dat by linear interpolation between each point
+  # this allows for finer-grained dat_shift offsets to fit experimental into expected
+  interpolated = []
+  gc_content_prev = 0
+  gc_content_amount_prev = 0
+  dat.each do |d|
+    gc_content = d[0]
+    gc_content_fine = (gc_content * $finesteps) - $finesteps # interpolating up from gc_content_prev, which is $finesteps prior
+    gc_content_amount = d[1].to_f
+    gc_content_amount_diff  = gc_content_amount - gc_content_amount_prev
+    #puts "interpolating for #{gc_content} #{gc_content_amount}"
+    0.upto($finesteps - 1) do |step|
+      gc_content_interp = gc_content_fine + step
+      #gc_content_amount_weight = 1 - ($finesteps - step).to_f/$finesteps
+      gc_content_amount_weight = step.to_f/$finesteps
+      gc_content_amount_interp = gc_content_amount_weight*gc_content_amount_diff + gc_content_amount_prev
+      #puts "  interp'd #{gc_content_interp} #{gc_content_amount_interp} with weight #{gc_content_amount_weight} on diff #{gc_content_amount_diff} plus prev #{gc_content_amount_prev}"
+      interpolated << [gc_content_interp, gc_content_amount_interp]
+    end
+    gc_content_prev = gc_content
+    gc_content_amount_prev = gc_content_amount
+  end
+  return interpolated
+end
+$expected_dat_interp = dat_interpolate($expected_dat)
+$experimental_dat_interp = dat_interpolate($experimental_dat)
+
 def dat_diff(expected, experimental)
   diffsquaressum = 0.0
 
   expected_amt = experimental_amt = nil
 
+  #min_gc = ([expected[0][0],  experimental[0][0]].min * $halffine).round
+  #max_gc = ([expected[-1][0], experimental[-1][0]].max * $halffine).round
   min_gc = [expected[0][0],  experimental[0][0]].min
   max_gc = [expected[-1][0], experimental[-1][0]].max
 
@@ -76,15 +109,17 @@ puts "expected:experimental diff, unadjusted: #{dat_diff($expected_dat, $experim
 
 def dat_shift(dat, shift)
   shifted = []
+  #interpolate = (shift.to_i != shift) # if using a non-integer shift, will need to interpolate values between two expected values.  if using integer shift, then shifted gc_contents should stay as integers
   dat.each do |d|
-    gc_content_adjusted = (d[0] + shift).to_i #XXX ok to do, this rounding?
+    gc_content_adjusted = (d[0] + shift)
+    #gc_content_adjusted = gc_content_adjusted.to_i if (!interpolate and gc_content_adjusted.to_i == gc_content_adjusted) # keep as int
     gc_content_amount   = d[1]
     shifted << [gc_content_adjusted, gc_content_amount]
   end
   return shifted
 end
-expected_centered     = dat_shift($expected_dat,     -$expected_mean)
-experimental_centered = dat_shift($experimental_dat, -$experimental_mean)
+expected_centered     = dat_shift($expected_dat,     -$expected_mean.to_i) # XXX: actually shift the interpolated dat
+experimental_centered = dat_shift($experimental_dat, -$experimental_mean.to_i) #XXX
 centered_diff = dat_diff(expected_centered, experimental_centered)
 puts "expected:experimental diff, centered:   #{dat_diff(expected_centered, experimental_centered)}"
 puts "expected_gc_content[i] =~ experimental_gc_content[i + (#{$expected_mean - $experimental_mean})]"
@@ -98,9 +133,9 @@ hi = offsets.max.ceil  + offset_diff - $experimental_mean.floor # reasonable upp
 #lo = -100
 #hi = 100
 experimental_shifted = nil
-lo.upto(hi) do |offset|
-  exper = dat_shift($experimental_dat, offset)
-  diff = dat_diff($expected_dat, exper)
+(lo*$finesteps).upto(hi*$finesteps) do |offset|
+  exper = dat_shift($experimental_dat_interp, offset)
+  diff = dat_diff($expected_dat_interp, exper)
   #puts "diff at #{offset} is #{diff}"
   if (diff < $best_diff)
     $best_offset = offset
@@ -109,8 +144,9 @@ lo.upto(hi) do |offset|
     #puts "best diff at #{offset} is #{diff}"
   end
 end
+
 puts "expected:experimental diff, found:      #{$best_diff}"
-puts "expected_gc_content[i] =~ experimental_gc_content[i + (#{$best_offset})]"
+puts "expected_gc_content[i] =~ experimental_gc_content[i + (#{$best_offset/$finesteps.to_f})]"
 
 def dat_max(dat)
   gc_content_amt_max = -(2**32)
@@ -141,5 +177,51 @@ end
 experimental_amped = dat_amplify(experimental_shifted, $amplification)
 experimental_amped_diff = dat_diff($expected_dat, experimental_amped)
 
+if (false) # debug
+  puts "exper orig:"
+  $experimental_dat.each do |d|
+    #printf("%i\t%f\n", d[0], d[1])
+    puts "#{d[0]}\t#{d[1]}"
+  end
+  puts "exper:"
+  $experimental_dat_interp.each do |d|
+    #printf("%i\t%f\n", d[0], d[1])
+    puts "#{d[0]}\t#{d[1]}"
+  end
+  puts "shifted:"
+  experimental_shifted.each do |d|
+    #printf("%i\t%f\n", d[0], d[1])
+    puts "#{d[0]}\t#{d[1]}"
+  end
+  puts "amped:"
+  experimental_amped.each do |d|
+    #printf("%i\t%f\n", d[0], d[1])
+    puts "#{d[0]}\t#{d[1]}"
+  end
+end
+
 puts "expected:experimental diff, amplified:  #{experimental_amped_diff}"
-puts "expected_gc_content[i] =~ experimental_gc_content[i + (#{$best_offset})] * (#{$amplification})"
+puts "expected_gc_content[i] =~ experimental_gc_content[i + (#{$best_offset.to_f / $finesteps})] * (#{$amplification})"
+
+#puts "debiased data follows according to above linear transformation:"
+#experimental_amped.each do |d|
+#  puts "#{d[0]}\t#{d[1]}"
+#end
+
+#puts "said data in original bins:"
+puts "debiased data follows according to above linear transformation:"
+idx = 0
+$experimental_dat.each do |ed|
+  gc_content = ed[0]
+  #puts "egcc #{gc_content}"
+  while (idx < experimental_amped.length)
+    #puts "?gcc #{experimental_amped[idx][0]} #{$finesteps}"
+    i = idx
+    idx += 1
+    if (experimental_amped[i][0] % $finesteps == 0)
+      #printf("%i\t%f\n", experimental_amped[i][0] / $finesteps, experimental_amped[i][1])
+      puts "#{experimental_amped[i][0] / $finesteps}\t#{experimental_amped[i][1]}"
+      break
+    end
+  end
+end
